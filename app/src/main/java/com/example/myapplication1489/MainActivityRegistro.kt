e23e922fabb77de958dadb4fefcffa5d40387b83
juan daniel torres moreno <kira00711@hotmail.com>
2019-07-11 13:04:23 -0500

    Lista con SQLite

---
+++ app/src/main/java/com/example/myapplication1489/MainActivityRegistro.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication1489
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main_registro.*
+import kotlinx.android.synthetic.main.content_main_activity_registro.*
+
+class MainActivityRegistro : AppCompatActivity() {
+
+    companion object{
+        val correo : String =""
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_registro)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+           if(etCorreoR.text.isEmpty() || etNombreR.text.isEmpty() || etPwdR.text.isEmpty() || etTelefono.text.isEmpty()){
+               Toast.makeText(this, "DATOS INCOMPLETOS", Toast.LENGTH_SHORT).show();
+               etCorreoR.requestFocus()
+           }
+            else{
+               val ema = etCorreoR.text.toString()
+               val nom = etNombreR.text.toString()
+               val pwd = etPwdR.text.toString()
+               val tel = etTelefono.text.toString()
+               val sentencia = "INSERT INTO Usuario( emailUsr,nomUsr,contUsr,telUsr) VALUES('$ema','$nom','$pwd','$tel')"
+               val admin = adminBD(this )
+               if (admin.Ejecuta(sentencia)==1){
+                   Toast.makeText(this, "Usuario Agregado con Exito", Toast.LENGTH_SHORT).show();
+               }else{
+                   Toast.makeText(this, "ERROR: No se pudo registrar", Toast.LENGTH_SHORT).show();
+               }
+           }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication1489/adminBD.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication1489
+
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class adminBD(context: Context) : SQLiteOpenHelper(context, DATABASE, null , 1)
+{
+    companion object{
+        val DATABASE = "Lista"
+
+    }
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(
+            "CREATE TABLE Usuario(" +
+                    "emailUsr text primary key," +
+                    " nomUsr text ," +
+                    " contUsr text," +
+                    "telUsr text)"
+        )
+        //db?.execSQL("CREATE TABLE")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    fun Ejecuta(sentencia : String):Int{
+        try {
+            val db = this.writableDatabase// se abre la base de datos en modo de lectura y escritura
+            db.execSQL(sentencia)
+            db.close()
+            return 1 //regresa 1 si la sentencia es exitosa
+        }catch (ex:Exception){
+            return 0
+        }
+    }
+
+    fun Consulta(select :String): Cursor?{
+        try {
+            val db = this.readableDatabase // abre solo en modo lectura no permite escribir
+            return db.rawQuery(select,null)
+        }catch(ex:Exception) {
+            return null
+        }
+    }
+}
